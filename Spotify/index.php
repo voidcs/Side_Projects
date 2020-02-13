ee5202e00164daa503c1ac06700e3bd938c0f503
voidcs <47105784+voidcs@users.noreply.github.com>
2020-02-13 12:17:57 -0800

    Add files via upload

---
+++ Spotify/index.php
@@ -0,0 +1,161 @@
+<?php
+	include ("includes/config.php");
+	//session_destroy();
+	
+	if(isset($_SESSION['userLoggedIn'])){
+		$userLoggedIn = $_SESSION['userLoggedIn'];
+	}
+	else{
+		header("Location: register.php");
+	}
+
+
+?>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>Welcome to Spotify</title>
+</head>
+<body>
+
+	<div id="mainContainer">
+
+		<div id="topContainer">
+			
+			<div id="navBarContainer">
+				<nav class="navBar">
+					
+					<a href="index.php" class="logo">
+						<img src="assets/images/icons/logo.png">
+					</a>
+
+
+					<div class="group">
+
+						<div class="navItem">
+							<a href="search.php" class="navItemLink">Search
+								<img src="assets/images/icons/search.png" class="icon" alt="Search">
+							</a>
+						</div>
+
+					</div>
+
+					<div class="group">
+
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+
+						<div class="navItem">
+							<a href="yourMusic.php" class="navItemLink">Your Music</a>
+						</div>
+
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Your Profile</a>
+						</div>
+
+					</div>
+
+
+				</nav>
+			</div>
+
+		</div>
+
+		<div id="nowPlayingBarContainer">
+			<div id="nowPlayingBar">
+				
+				<div id="nowPlayingLeft">
+					<div class="content">
+						<span class="albumLink">
+							<img src="https://upload.wikimedia.org/wikipedia/en/b/b8/Twice-Merry_%26_Happy_%28album_cover%29.jpg" class= "albumArtwork">
+						</span>
+
+						<div class="trackInfo">
+							<span class="trackName">
+								<span>Heart shaker</span>
+							</span>
+
+							<span class="artistName">
+								<span>Twice</span>
+							</span>
+						</div>
+
+
+					</div>
+				</div>
+
+				<div id="nowPlayingCenter">
+					
+					<div class="content playerControls">
+						
+						<div class="buttons">
+							
+							<button class = "controlButton shuffle" title="Shuffle button">
+								<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+							</button>
+
+							<button class = "controlButton previous" title="Previous button">
+								<img src="assets/images/icons/previous.png" alt="Previous">
+							</button>
+
+							<button class = "controlButton play" title="Play button">
+								<img src="assets/images/icons/play.png" alt="Play">
+							</button>
+
+							<button class = "controlButton pause" title="Pause button" style="display: none;">
+								<img src="assets/images/icons/pause.png" alt="Pause">
+							</button>
+
+							<button class = "controlButton next" title="Next button">
+								<img src="assets/images/icons/next.png" alt="Next">
+							</button>
+
+							<button class = "controlButton repeat" title="Repeat button">
+								<img src="assets/images/icons/repeat.png" alt="Repeat">
+							</button>
+
+						</div>
+
+						<div class="playbackBar">
+							
+							<span class="progressTime current">0.00</span>
+
+							<div class="progressBar">
+								<div class="progressBarBg">
+									<div class="progress"></div>
+								</div>
+							</div>
+
+							<span class="progressTime remaining">0.00</span>
+
+						</div>
+					</div>
+
+				</div>
+
+				<div id="nowPlayingRight">
+					<div class="volumeBar">
+						<button class="controlButton volume" title="Volume button">
+							<img src="assets/images/icons/volume.png" alt="Volume">
+						</button>
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+
+
+
+
+
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
