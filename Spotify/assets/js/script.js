508863fe2ea728d2e116b72d8b6bfef7e2a0c0d2
voidcs <47105784+voidcs@users.noreply.github.com>
2020-02-20 00:13:06 -0800

    Add files via upload

--- Spotify/assets/js/script.js
+++ Spotify/assets/js/script.js
@@ -8,8 +8,13 @@ var currentIndex = 0;
 var repeat = false;
 var shuffle = false;
 var userLoggedIn;
+var timer;
 
 function openPage(url){
+
+	if(timer != null){
+		clearTimeout(timer);
+	}
 
 	if(url.indexOf("?") == -1){
 		url = url + "?";
@@ -40,6 +45,10 @@ function updateTimeProgressBar(audio){
 function updateVolumeProgressBar(audio){
 	var volume = audio.volume * 100;
 	$(".volumeBar .progress").css("width", volume + "%");
+}
+
+function playFirstSong(){
+	setTrack(tempPlaylist[0], tempPlaylist, true);
 }
 
 function Audio(){
