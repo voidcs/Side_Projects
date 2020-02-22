aa4a8aef4fca1bc32e8456bbe1b930cfd09883b5
voidcs <47105784+voidcs@users.noreply.github.com>
2020-02-21 22:51:32 -0800

    Add files via upload

--- Spotify/artist.php
+++ Spotify/artist.php
@@ -52,7 +52,8 @@ $artist = new Artist($con, $artistId);
 					</div>
 
 					<div class='trackOptions'>
-						<img class='optionsButton' src='assets/images/icons/more.png'>
+						<input type='hidden' class='songId' value='". $albumSong->getId() ."'>
+						<img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
 					</div>
 
 					<div class='trackDuration'>
@@ -95,3 +96,8 @@ $artist = new Artist($con, $artistId);
 	?>
 
 </div>
+
+<nav class="optionsMenu">
+	<input type="hidden" class="songId">
+	<?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+</nav>
